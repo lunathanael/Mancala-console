a3c7dbbecf7b21102c4c90153cfb52379626bb25
lunathanael <68858103+lunathanael@users.noreply.github.com>
2023-06-19 19:03:10 -0400

    Added sowing and captures

--- defs.h
+++ defs.h
@@ -3,6 +3,8 @@
 
 #define NUMBER_OF_HOUSES_PER_SIDE 6
 #define NUMBER_OF_PLAYERS 2
+#define STARTING_SEEDS_PER_HOUSE 4
+static constexpr int ALLOW_CAPTURES = 1;
 
 static constexpr int NUMBER_OF_TOTAL_HOUSES = (2 * NUMBER_OF_HOUSES_PER_SIDE);
 static constexpr int NUMBER_OF_TOTAL_HOLES = (NUMBER_OF_TOTAL_HOUSES + NUMBER_OF_PLAYERS);
@@ -21,11 +23,25 @@ STORE (B)
 								STORE (A)
 */
 
-enum PLAYERS{ PLAYER_A, PLAYER_B};
+enum PLAYERS{ 
+	PLAYER_A, PLAYER_B
+};
 
 
-typedef struct BOARD { unsigned int STONES_ON_HOLE[NUMBER_OF_TOTAL_HOLES] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; } BOARD;
+enum HOLE_REFERENCE {
+	A1, A2, A3, A4, A5, A6, B1, B2, B3, B4, B5, B6
+};
 
+
+constexpr int get_hole_side(int hole) {
+	return (hole / 6);
+};
+
+
+struct GAMESTATE {
+	unsigned int board[NUMBER_OF_TOTAL_HOLES] = {};
+	int current_player = PLAYER_A;
+};
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "defs.h"
+#include <algorithm>
 
 using namespace std;
+
 
 void print_number(int number) {
 	cout << number;
@@ -12,7 +14,7 @@ void print_number(int number) {
 }
 
 
-void print_board(BOARD* board) {
+void print_board(GAMESTATE* gamestate) {
 	int current_hole_index;
 
 
@@ -23,18 +25,18 @@ void print_board(BOARD* board) {
 	current_hole_index = PLAYER_TO_STORE_INDEX[PLAYER_B] - 1;
 	for (; current_hole_index > PLAYER_TO_STORE_INDEX[PLAYER_A]; --current_hole_index) {
 		cout << "|";
-		print_number(board->STONES_ON_HOLE[current_hole_index]);
+		print_number(gamestate->board[current_hole_index]);
 	}
 	cout << "|   |" << endl;
 
 	// Print Stores
 	cout << "|";
-	if (board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_B]] < 10) {
+	if (gamestate->board[PLAYER_TO_STORE_INDEX[PLAYER_B]] < 10) {
 		cout << ' ';
 	}
-	cout << board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_B]];
+	cout << gamestate->board[PLAYER_TO_STORE_INDEX[PLAYER_B]];
 	cout << " +--+--+--+--+--+--+ ";
-	print_number(board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_A]]);
+	print_number(gamestate->board[PLAYER_TO_STORE_INDEX[PLAYER_A]]);
 	cout << "|" << endl;
 
 	// Print player A side
@@ -42,7 +44,7 @@ void print_board(BOARD* board) {
 	current_hole_index = 0;
 	for (; current_hole_index < PLAYER_TO_STORE_INDEX[PLAYER_A]; ++current_hole_index) {
 		cout << "|";
-		print_number(board->STONES_ON_HOLE[current_hole_index]);
+		print_number(gamestate->board[current_hole_index]);
 	}
 	cout << "|   |" << endl;
 
@@ -51,9 +53,75 @@ void print_board(BOARD* board) {
 	return;
 }
 
+
+void start_game(GAMESTATE* gs) {
+	int current_hole_index;
+
+	gs->current_player = PLAYER_A;
+
+	// Setup board
+	current_hole_index = 0;
+	
+	fill_n(&gs->board[current_hole_index], NUMBER_OF_HOUSES_PER_SIDE, STARTING_SEEDS_PER_HOUSE);
+
+
+	current_hole_index = PLAYER_TO_STORE_INDEX[PLAYER_A] + 1;
+
+	fill_n(&gs->board[current_hole_index], NUMBER_OF_HOUSES_PER_SIDE, STARTING_SEEDS_PER_HOUSE);
+	
+	return;
+}
+
+
+void do_capture(GAMESTATE* gs, int hole_index) {
+	int opposite_hole_index = ((2 * PLAYER_TO_STORE_INDEX[PLAYER_A]) - hole_index);
+	int seed_count = gs->board[opposite_hole_index];
+	gs->board[PLAYER_TO_STORE_INDEX[gs->current_player]] += seed_count;
+	gs->board[opposite_hole_index] = 0;
+	return;
+}
+
+
+bool sowing(GAMESTATE*gs, int hole_index) {
+	int seed_count = gs->board[hole_index];
+	gs->board[hole_index] = 0;
+	while (seed_count > 0) {
+		++hole_index;
+
+		if (hole_index == NUMBER_OF_TOTAL_HOLES) {
+			hole_index = 0;
+		}
+		int opposite_player_index = (gs->current_player ^ 1);
+		if (hole_index == PLAYER_TO_STORE_INDEX[opposite_player_index]) {
+			continue;
+		}
+
+		++gs->board[hole_index];
+		--seed_count;
+	}
+
+
+	if (hole_index = PLAYER_TO_STORE_INDEX[gs->current_player]) {
+		return true;
+	}
+	else if (ALLOW_CAPTURES) {
+		if (gs->board[hole_index] == 1) {
+			do_capture(gs, hole_index);
+		}
+	}
+
+	return false;
+}
+
+int game_cycle(GAMESTATE*gs) {
+	return 0;
+}
+
+
 int main() {
-	BOARD new_board;
-	print_board(&new_board);
+	GAMESTATE new_game;
+	start_game(&new_game);
+	print_board(&new_game);
 
 	return 0;
 }
