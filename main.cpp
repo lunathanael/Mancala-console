fcf8aca5aad8723d1ad34baf580d41c2996bc3e8
lunathanael <68858103+lunathanael@users.noreply.github.com>
2023-06-19 18:19:19 -0400

    print board

--- main.cpp
+++ main.cpp
@@ -3,21 +3,52 @@
 
 using namespace std;
 
+void print_number(int number) {
+	cout << number;
+	if (number < 10) {
+		cout << ' ';
+	}
+	return;
+}
+
 
 void print_board(BOARD* board) {
 	int current_hole_index;
 
+
+	cout << "+---+--+--+--+--+--+--+---+" << endl;
+
 	// Print player B side
-	cout << "  ";
+	cout << "|   ";
 	current_hole_index = PLAYER_TO_STORE_INDEX[PLAYER_B] - 1;
 	for (; current_hole_index > PLAYER_TO_STORE_INDEX[PLAYER_A]; --current_hole_index) {
-		cout << board->STONES_ON_HOLE[current_hole_index] << ' ';
+		cout << "|";
+		print_number(board->STONES_ON_HOLE[current_hole_index]);
 	}
-	cout << endl;
+	cout << "|   |" << endl;
 
 	// Print Stores
-	cout << board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_B]] << "                " << board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_A]] << endl;
+	cout << "|";
+	if (board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_B]] < 10) {
+		cout << ' ';
+	}
+	cout << board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_B]];
+	cout << " +--+--+--+--+--+--+ ";
+	print_number(board->STONES_ON_HOLE[PLAYER_TO_STORE_INDEX[PLAYER_A]]);
+	cout << "|" << endl;
+
 	// Print player A side
+	cout << "|   ";
+	current_hole_index = 0;
+	for (; current_hole_index < PLAYER_TO_STORE_INDEX[PLAYER_A]; ++current_hole_index) {
+		cout << "|";
+		print_number(board->STONES_ON_HOLE[current_hole_index]);
+	}
+	cout << "|   |" << endl;
+
+	cout << "+---+--+--+--+--+--+--+---+" << endl;
+
+	return;
 }
 
 int main() {
